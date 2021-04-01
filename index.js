7ba24b6f0c5ea4faef3479f549694fd1169dfb4f
+ <+>
2021-04-01 11:19:39 -0600

    commit tree 97853ad6e67b8e8c09df8c905b4e2cebeaa74045

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Your code here
+function createEmployeeRecord([first, last, job, pay]){
+    const newEmployee = {
+        firstName: first, 
+        familyName: last, 
+        title: job, 
+        payPerHour: pay, 
+        timeInEvents: [], 
+        timeOutEvents: [] 
+    }
+    return newEmployee
+}
+
+function createEmployeeRecords(arryOne){
+    return arryOne.map(x => createEmployeeRecord(x))
+}
+
+function createTimeInEvent(object, timeStamp){
+    let splitTimeStamp = timeStamp.split(' ')
+    let hour = splitTimeStamp[1]
+    let date = splitTimeStamp[0]
+    object.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return object
+}
+
+function createTimeOutEvent(object, timeStamp){
+    let splitTimeStamp = timeStamp.split(' ')
+    let hour = splitTimeStamp[1]
+    let date = splitTimeStamp[0]
+    object.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date: date
+    })
+    return object
+}
+
+function hoursWorkedOnDate(object, date) {
+    const timeInArry = object.timeInEvents
+    const timeOutArry = object.timeOutEvents
+    const findClockInDate = timeInArry.find(element => element.date === date)
+    const findClockOutDate = timeOutArry.find(element => element.date === date)
+    const clockInHour = Object.keys(findClockInDate)[1]
+    const clockOutHour = Object.keys(findClockOutDate)[1]
+    if (clockInHour > clockOutHour){
+        return clockInHour - clockOutHour
+    } else if (clockOutHour > clockInHour) {
+        return clockOutHour - clockInHour
+    }
+
+    console.log(Object.keys(findClockInDate)[1])
+    console.log(Object.keys(findClockOutDate)[1])
+    return object
+}
